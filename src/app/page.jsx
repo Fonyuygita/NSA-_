b3f9d556f9fb6504515c9738840a231ebb897f3c
Fonyuy Gita <FonyuyJudegita@gmail.com>
2024-01-30 01:38:38 -0800

    moving to our services section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "./home.module.css";
 import Hero from "@/components/home/hero/Hero";
+import Services from "@/components/services/Services";
 
 const Home = () => {
   return (
     <section>
       <Hero/>
+      <Services/>
     </section>
      );
 };
---
+++ src/components/services/Services.jsx
@@ -0,0 +1,12 @@
+
+import styles from "./services.module.css"
+
+const Services=()=>{
+    return(
+        <div className={styles.container}>
+            <h1>Our Services</h1>
+        </div>
+    )
+}
+
+export default Services
