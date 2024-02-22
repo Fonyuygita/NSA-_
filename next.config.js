fe7f45287e8229092a9d1db70889e7621f027e94
Fonyuy Gita <FonyuyJudegita@gmail.com>
2024-02-22 00:19:32 -0800

    added contact page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    swcMinify:false
+    swcMinify:false,
+    // Enable the Next.js Image Optimization API
+    images: {
+        // Specify the domains that are allowed to serve images
+        domains: ["images.pexels.com"],
+        // Specify the loader to use for image optimization
+        loader: "default",
+      },
 }
 
 module.exports = nextConfig
+
+
+
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,13 +1,45 @@
-import styles from "./contact.module.css"
-
+// pages/contact.js
+import React from "react";
+import Image from "next/image";
+import styles from "./contact.module.css";
 
 const contactPage=()=>{
-    return(
-        <div className={styles.container}>
-            <h1>Contact page</h1>
-            <p>On Construction</p>
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Contact Us</h1>
+      <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, cum repellat? Totam.</p>
+      <div className={styles.contactBox}>
+        <div className={styles.form}>
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" name="name" placeholder="Your name" />
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Your email address"
+          />
+          <label htmlFor="message">Message</label>
+          <textarea
+            id="message"
+            name="message"
+            placeholder="Write something.."
+          ></textarea>
+          <button type="submit">Send</button>
         </div>
-    )
+        <div className={styles.image}>
+          <Image
+            src="/contactUs.png"
+            alt="A person holding a phone with a contact icon"
+            width={200}
+            height={300}
+            objectFit="cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
+
 
 export default contactPage
