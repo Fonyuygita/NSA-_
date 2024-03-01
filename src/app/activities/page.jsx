1c57da842df94eed76f753fc84df4e38ef5d1b64
Fonyuy Gita <FonyuyJudegita@gmail.com>
2024-03-01 13:57:02 -0800

    no eslintsss rules

--- src/app/activities/page.jsx
+++ src/app/activities/page.jsx
@@ -5,7 +5,7 @@ import CategoryButton from './CategoryButton'
 import Activity from '@/components/activity/Activity'
 import SectionHeader from '@/components/sectionHeader/sectionHeader'
 
-const activityPage=()=>{
+const ActivityPage=()=>{
 
     const [projects, setProjects]=useState(Data)
     // console.log(projects);
@@ -43,4 +43,4 @@ return;
     )
 }
 
-export default activityPage
+export default ActivityPage
