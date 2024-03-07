cfcde72f0aa7769d764d8a8dbe386572c1bc7a37
Fonyuy Gita <FonyuyJudegita@gmail.com>
2024-03-06 22:40:27 -0800

    added support buton

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -8,6 +8,7 @@ import Link from "next/link"
 import SectionHeader from "@/components/sectionHeader/sectionHeader"
 import Faqs from "@/components/faqs/Faqs.jsx"
 import { useState } from "react"
+import { FaMoneyBillTransfer } from "react-icons/fa6"
 
 const AboutPage=()=>{
 
@@ -29,7 +30,6 @@ const handleReadMore=()=>{
         Noni Students’ Association is an umbrella group of all students and learners from Noni Subdivision of Bui Division in the North West Region of Cameroon. Created in December 1960 in Djottin Noni, the Association had as pioneer president Hon. Konglimnyuy Joseph Nkwi and as current General President Mr. Ndiyun Robert Kosho. The association operates through federal system, with the General Assembly as the main decision-making organ, the General Executive Bureau vested with the powers to oversee the smooth functioning of the association. At the lower level there exist semi-autonomous branches spread across the principal villages of Noni, the major cities of Cameroon and in the diaspora, with each branch having its general assembly and executive bureau under the leadership of a branch president
         </p>
 
-        <Link href="../assets/cv.pdf" className={`btn ${styles.aboutSupport}`} download rel="noopener noreferrer">Support Us</Link>
     </article>
 
         </div>
@@ -48,7 +48,6 @@ const handleReadMore=()=>{
         }
         
 
-
 <div className={styles.nsaLogo}>
 
 <Image src="/logo (2).png" width={100} height={100} alt="logo" className={styles.float} object-fit="cover"/>
@@ -62,6 +61,13 @@ const handleReadMore=()=>{
         </div>
 
         <Faqs/>
+        <div className={styles.supportButton}>
+<Link href="" className={styles.reactIcon}>
+<FaMoneyBillTransfer color="black" size={30}/>
+</Link>
+<span>Support</span>
+
+</div>
        </section>
     )
 }
