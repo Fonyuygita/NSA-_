f7c59b9a42e91ff1c7c036762971fb8cd64a27f1
Fonyuy Gita <FonyuyJudegita@gmail.com>
2024-03-22 07:53:43 -0700

    seo stuffs

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,10 +5,14 @@ import Services from "@/components/services/Services";
 import Executives from "@/components/executive/Executive";
 import Objectives from "@/components/objectives/Objectives";
 import TestimonialsPage from "@/components/testimonial/Testimonial.jsx";
+import Head from "next/head";
 
 const Home = () => {
   return (
     <section className={styles.content}> 
+    <Head>
+    <meta name="google-site-verification" content="2tcesPtaKPl4hu1Lvz0ICM0phoIgcH-HXRv7SffXr9U" />
+  </Head>
       <Hero/>
       <Executives/>
       <Services/>
