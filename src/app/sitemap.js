75d9a67fa32b564059a4d51e52ab10397877a574
Fonyuy Gita <FonyuyJudegita@gmail.com>
2024-04-24 00:08:02 -0700

    merge to master and enhanced seo

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,28 +1,49 @@
-export default function sitemap() {
-    return [
-      {
-        url: process.env.NEXT_PUBLIC_BASE_URL,
-        lastModified: new Date(),
-        changeFrequency: 'yearly',
-        priority: 1,
-      },
-      {
-        url: `${process.env.NEXT_PUBLIC_BASE_URL}/about`,
-        lastModified: new Date(),
-        changeFrequency: 'monthly',
-        priority: 0.8,
-      },
-      {
-        url:  `${process.env.NEXT_PUBLIC_BASE_URL}/activities`,
-        lastModified: new Date(),
-        changeFrequency: 'weekly',
-        priority: 0.5,
-      },
-      {
-        url:  `${process.env.NEXT_PUBLIC_BASE_URL}/contact`,
-        lastModified: new Date(),
-        changeFrequency: 'weekly',
-        priority: 0.5,
-      },
-    ]
-  }
+
+ 
+export default async function sitemap(){
+  const defaultPages = [
+    {
+      url: "https://nonistudents.vercel.app/",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1
+    },
+    {
+      url: "https://nonistudents.vercel.app/about",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9
+    },
+    {
+      url: "https://nonistudents.vercel.app/activities",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9
+    },
+    {
+      url: "https://nonistudents.vercel.app/contact",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9
+    },
+
+    {
+      url: "https://nonistudents.vercel.app/branches",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9
+    }
+
+    // other pages
+  ];
+ 
+
+ 
+  const sitemap = [
+    ...defaultPages,
+    
+    
+  ];
+ 
+  return sitemap;
+}
