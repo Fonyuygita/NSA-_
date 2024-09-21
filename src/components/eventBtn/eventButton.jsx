3e16cdb6be6b7b68161d79c71e9c9bee09c7b273
Fonyuy Gita <FonyuyJudegita@gmail.com>
2024-09-21 02:36:34 -0700

    fixing merge conflict

--- src/components/eventBtn/eventButton.jsx
+++ src/components/eventBtn/eventButton.jsx
@@ -7,11 +7,7 @@ const FixedButton = () => {
   return (
     <div className={styles.fixedButton}>
       <Link href="/event" className={styles.buttonLink}>
-<<<<<<< HEAD
         Next Event 😑
-=======
-        Next Event
->>>>>>> fac371fa7fc8a42ae14e85a33a75d92f6ac71579
       </Link>
     </div>
   );
